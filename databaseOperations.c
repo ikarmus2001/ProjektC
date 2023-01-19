21cc72c7d4b7b9a5ca44e2e3e8e349942352c71e
Kacper Hałaczkiewicz <kahalaczkiewicz@wp.pl>
2023-01-19 09:14:58 +0100

    Naprawiono subskrypcje OnClick na przyciskach, szkielet obsługi plików i okno wyboru pliku

--- databaseOperations.c
+++ databaseOperations.c
@@ -3,16 +3,12 @@
 #include <gtk/gtk.h>
 
 typedef struct cokolwiek{
-    char* cosss[100];
-    int v;
+    char* nazwa;
+    int ilosc;
+    float wartosc;
 } JakasStruktura;
 
 static GtkWidget* fillWarehouse(JakasStruktura* js) {
-
-
-    
-    
-    fclose(file);
 
     // int rowsCount = sizeof(js)/sizeof(js[0]);
     // for (int i = 0; i < rowsCount; i++) {
@@ -22,22 +18,67 @@ static GtkWidget* fillWarehouse(JakasStruktura* js) {
         //     column
         // );
     // }
+    GtkWidget* listbox = gtk_list_box_new();
 
-    // return resultTreeView;
+    return listbox;
 }
 
-static void showItems(GtkWidget** ) {
-    GtkWidget* listBox = createListbox();
-    
-    fillListBox(listBox, );
-    gtk_list_box_insert();
+// Otwiera nowe okno, w którym uzytkownik wskazuje plik z danymi,
+// a następnie otwiera go jako strumień
+FILE* selectAndOpenFile() {  //GtkWindow* parentWindow) {
+    printf("Selecting file started");
+
+    GtkWidget *dialog;
+    GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_OPEN;
+    gint res;
+    FILE* fs = NULL;
+
+    dialog = gtk_file_chooser_dialog_new ("Open File",
+                                        NULL, //parentWindow,
+                                        action,
+                                        "Cancel",
+                                        GTK_RESPONSE_CANCEL,
+                                        "Open",
+                                        GTK_RESPONSE_ACCEPT,
+                                        NULL);
+
+    res = gtk_dialog_run (GTK_DIALOG (dialog));
+    if (res == GTK_RESPONSE_ACCEPT) {
+        char *filename;
+        GtkFileChooser *chooser = GTK_FILE_CHOOSER (dialog);
+        filename = (char*)gtk_file_chooser_get_filename (chooser);
+        fs = fopen(filename, "r");
+    }
+
+    gtk_widget_destroy (dialog);
+
+    printf("Selecting file ended, file_chooser destroyed");
+
+    return fs;
 }
 
-// Todo zmienić "read from file" na "read" 
-// - w myśl mvvm
+// Sprawdza poprawność danych do utworzenia struktury, 
+// zwraca zainicjowaną i wypełnioną danymi strukturę, 
+// w przypadku błędu zwraca NULL
+JakasStruktura* checkLine(char* str) {
+    char s1;
+    char readString[100];
+    int d1;
+    float f1;
+    sscanf(str, "%s;%d;%f", &s1, &d1, &f1);
+    strcpy(readString, &s1);
+    if (d1 >= 12 && readString[0] != '\0' && f1 > 0 && f1 < 1) {
+        JakasStruktura* newLine = (JakasStruktura*)malloc(sizeof(JakasStruktura));
+        newLine->nazwa = readString;
+        newLine->ilosc = d1;
+        newLine->wartosc = f1;
+        return newLine;
+    }
+    return NULL;
+}
 
-int countLines(FILE* file)
-{
+// Sprawdza ilość linijek wewnątrz otwartego pliku
+int countLines(FILE* file) {
     // https://stackoverflow.com/a/70708991/14345698
 
     // TODO sprawdzać wielkość pliku zanim odczytam ilość linijek,
@@ -65,32 +106,33 @@ int countLines(FILE* file)
 }
 
 
-void readFromFile() {
-    FILE* filestream = selectFile();
-    int fileLength = countLines(filestream);
-
-    JakasStruktura* js = createStructsFromFile(filestream, fileLength);
-
-    GtkWidget* listBox = fillWarehouse();
-}
-
 JakasStruktura* createStructsFromFile(FILE* filestream, size_t fileLength){
-    JakasStruktura* js = malloc(fileLength * sizeof(JakasStruktura));
+    JakasStruktura* js = (JakasStruktura*)malloc(fileLength * sizeof(JakasStruktura));
     JakasStruktura* currentItem;
     size_t failed = 0;
     size_t j = 0;
+    unsigned int buffer_size = 500;
+    char buffer[buffer_size];
     for (size_t i = 0; i < fileLength; i++) {
-
-        if ((currentItem = checkLine(currentLine)) != NULL) {
-            js[j] = currentItem;
+        fgets(buffer, buffer_size, filestream);
+        puts(buffer);
+        if ((currentItem = checkLine(buffer)) != NULL) {
+            js[j] = *currentItem;
             j++;
         }
         else
-            failed++;
+            failed++;  // todo obsługa?
     }
     return js;
 }
 
-    
-    //GtkWidget* listBox = fillWarehouse(js);
-    // showItems(listBox);
+void readFromFile() {
+    FILE* filestream = selectAndOpenFile();
+    int fileLength = countLines(filestream);
+
+    JakasStruktura* js = createStructsFromFile(filestream, fileLength);
+    fclose(filestream);
+
+
+    GtkWidget* listBox = fillWarehouse(js);
+}
--- windowPrepare.c
+++ windowPrepare.c
@@ -18,16 +18,16 @@ static GtkWidget* setupWindow(GtkApplication *app, gpointer user_data) {
 }
 
 // Tworzy obiekt przycisku i nadaje mu zadane właściwości
-static GtkWidget* setupButton(char* label)
-{
+static GtkWidget* setupButton(char* label) {
     GtkWidget *btn = gtk_button_new_with_label(label);
     return btn;
 }
 
-// Todo przerobić na MVVM
-
 
 static void readFromFile_OnClick(GtkWidget *widget, gpointer data) {
+    fprintf(stdout, "Nie wiem właściwie gdzie to się wypisze\n");
+    fprintf(stderr, "STD ERR\n");
+    fflush(stdout);
     readFromFile();
 }
 
@@ -41,14 +41,24 @@ static void saveToFile_OnClick(GtkWidget *widget, gpointer data) {
 
 // Dodaje funkcje do zdarzeń
 static void addEventSubscriptions(
-    GtkWidget** readFromFile_btn, 
-    GtkWidget** addItem_btn, 
-    GtkWidget** saveToFile_btn,
-    GtkWidget** window) {
+        GtkWidget* readFromFile_btn, 
+        GtkWidget* addItem_btn, 
+        GtkWidget* saveToFile_btn,
+        GtkWidget* window) {
+
+    printf("Connecting button handlers start\n");
 
     g_signal_connect(readFromFile_btn, "clicked", G_CALLBACK(readFromFile_OnClick), NULL);
+    printf("After connecting onclick handler readFromFile\n");
+
+
     g_signal_connect(addItem_btn, "clicked", G_CALLBACK(addItem_OnClick), NULL);
+    printf("After connecting onclick handler addItem_btn\n");
+
     g_signal_connect(saveToFile_btn, "clicked", G_CALLBACK(saveToFile_OnClick), NULL);
+    printf("After connecting onclick handler saveToFile_btn\n");
+
+    printf("Connecting button handlers end\n");
 }
 
 void handleError(GError **err) {
@@ -64,6 +74,7 @@ static GtkWidget* createMainTreeView() {
 
 // Ustawia elementy UI
 void activate(GtkApplication *app, gpointer user_data) {
+
     GtkWidget *window = setupWindow(app, &user_data);
 
     GtkWidget *menuHorizontalBox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
@@ -75,6 +86,8 @@ void activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *addItem_btn = setupButton("Dodaj <coś>");
     GtkWidget *saveToFile_btn = setupButton("Zapisz");
 
+    fprintf(stdout, "Po utworzeniu przycisków\n");
+
     //setupContainers();
     gtk_box_pack_start(GTK_BOX(menuHorizontalBox), mainTreeView, 0, 0, 0);
     gtk_box_pack_end(GTK_BOX(menuHorizontalBox), menuVerticalBox, 0, 0, 0);
@@ -82,9 +95,17 @@ void activate(GtkApplication *app, gpointer user_data) {
     gtk_box_pack_start(GTK_BOX(menuVerticalBox), addItem_btn, 0, 0, 0);
     gtk_box_pack_start(GTK_BOX(menuVerticalBox), saveToFile_btn, 0, 0, 0);
 
-    addEventSubscriptions(&readFromFile_btn, &addItem_btn, &saveToFile_btn, &window);
+    fprintf(stdout, "Po zapakowaniu buttonów\n");
+
+    addEventSubscriptions(readFromFile_btn, addItem_btn, saveToFile_btn, window);
     // gtk_container_add(GTK_CONTAINER(window), button);
     gtk_container_add(GTK_CONTAINER(window), menuHorizontalBox);
+
+    fprintf(stdout, "po dodaniu containera");
+
     gtk_widget_show_all(window);
+
+    fprintf(stdout, "po pokazaniu widgetów");
+
     
 }
